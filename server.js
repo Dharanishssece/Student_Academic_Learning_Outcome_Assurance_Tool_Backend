d14919f528a6b1c621df62199b839508afb31f74
Dharanishssece <dharanishssece@gmail.com>
2026-04-19 20:20:11 +0530

    updated cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174", process.env.CORS_ORIGIN],
   credentials: true
 }));
 app.use(express.json());
